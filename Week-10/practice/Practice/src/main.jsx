1e9ca34d34814baeada3124d634526c194b37839
Faisal Junaid <ayanp3821@gmail.com>
2024-04-30 15:04:05 +0500

    Counter added

--- Week-10/practice/Practice/src/main.jsx
+++ Week-10/practice/Practice/src/main.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Apps from './name.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Apps />
+    <App />
   </React.StrictMode>
 );
